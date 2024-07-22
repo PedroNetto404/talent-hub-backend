7a98a3f1749e8d08ee8c95ecb09e691dc56cee0f
Pedro Netto <103741225+PedroNetto404@users.noreply.github.com>
2024-07-22 14:43:00 -0300

    feat(Entity): add equality comparers

--- src/TalentHub.Domain/Core/Primitives/Entity.cs
+++ src/TalentHub.Domain/Core/Primitives/Entity.cs
@@ -10,8 +10,27 @@ public abstract record EntityId<TId>
     public static implicit operator EntityId<TId>(Guid value) => new TId { Value = value };
 }
 
-public abstract class Entity<TEntityId>
+public abstract class Entity<TEntityId> : IEquatable<Entity<TEntityId>>
     where TEntityId : EntityId<TEntityId>, new()
 {
     public TEntityId Identification { get; } = new();
-} 
+
+    public bool Equals(Entity<TEntityId>? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Identification.Value == other.Identification.Value;
+    }
+
+    public override bool Equals(object? obj) =>
+        ReferenceEquals(this, obj) || obj is Entity<TEntityId> other && Equals(other);
+
+    public override int GetHashCode() =>
+        Identification.Value.GetHashCode() * 397;
+    
+    public static bool operator ==(Entity<TEntityId>? left, Entity<TEntityId>? right) =>
+        Equals(left, right);
+    
+    public static bool operator !=(Entity<TEntityId>? left, Entity<TEntityId>? right) =>
+        !(left == right);
+}
